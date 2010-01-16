1fee21a03e22db34a75a79ebafa9e725fdac44c9
rkaminski <rkaminski@c2a30a28-f9ed-4af8-86dc-e4e425da2d4f>
2010-01-16 19:46:11 +0000

    - added some quotes to allow for spaces in filenames   - this wont allow for every thinkable but should capture all relevant cases
    git-svn-id: http://svn.code.sf.net/p/potassco/code/trunk/benchmark-tool@2101 c2a30a28-f9ed-4af8-86dc-e4e425da2d4f

--- templates/seq-generic.sh
+++ templates/seq-generic.sh
@@ -1,12 +1,12 @@
 $!"#"+"!/bin/bash"!$
 
-cd $(dirname $0)
+cd "$(dirname $0)"
 
-$!run.root()!$/programs/runsolver \
+"$!run.root()!$/programs/runsolver-3.2.5" \
 	-w runsolver.watcher \
 	-o runsolver.solver \
 	1> runsolver.stderr \
 	2> runsolver.stdout \
 	-W $!run.timeout().seconds()!$ \
-	$!run.root()!$/programs/$!run.solver()!$ $!run.args()!$ \
-	< $!run.file()!$
+	"$!run.root()!$/programs/$!run.solver()!$" $!run.args()!$ \
+	< "$!run.file()!$"
