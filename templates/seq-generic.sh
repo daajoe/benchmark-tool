e9b87bf93020e3c6d428f28ac9f9abaab2c6ad63
rkaminski <rkaminski@c2a30a28-f9ed-4af8-86dc-e4e425da2d4f>
2010-01-28 13:08:13 +0000

    - added a lockfile to prevent benchmarks from running twice
    git-svn-id: http://svn.code.sf.net/p/potassco/code/trunk/benchmark-tool@2184 c2a30a28-f9ed-4af8-86dc-e4e425da2d4f

--- templates/seq-generic.sh
+++ templates/seq-generic.sh
@@ -2,7 +2,7 @@ $!"#"+"!/bin/bash"!$
 
 cd "$(dirname $0)"
 
-"$!run.root()!$/programs/runsolver-3.2.5" \
+[[ .finished -nt start.sh ]] || "$!run.root()!$/programs/runsolver-3.2.5" \
 	-w runsolver.watcher \
 	-o runsolver.solver \
 	1> runsolver.stderr \
@@ -10,3 +10,5 @@ cd "$(dirname $0)"
 	-W $!run.timeout()!$ \
 	"$!run.root()!$/programs/$!run.solver()!$" $!run.args()!$ \
 	< "$!run.file()!$"
+
+touch .finished
