b9a5dc9b31831c1a483fda1c66ea2ebdef201ed6
rkaminski <rkaminski@c2a30a28-f9ed-4af8-86dc-e4e425da2d4f>
2010-03-23 15:08:51 +0000

    git-svn-id: http://svn.code.sf.net/p/potassco/code/trunk/benchmark-tool@2449 c2a30a28-f9ed-4af8-86dc-e4e425da2d4f

--- templates/pbs-generic.sh
+++ templates/pbs-generic.sh
@@ -8,6 +8,7 @@ function abort()
 
 function run()
 {{
+	# does not work because this will only kill mpiexec
 	trap abort SIGUSR1
 	/usr/bin/time -f "Real time (s): %e" -o runsolver.watcher \
 	mpiexec -machinefile $PBS_NODEFILE -n $PBS_NODES \
@@ -22,8 +23,26 @@ function run()
 	kill $sleep_pid
 }}
 
-cd "$(dirname $0)"
+function run2()
+{{
+	MPIEXEC_TIMEOUT={run.timeout} \
+		mpiexec -machinefile $PBS_NODEFILE -n $PBS_NODES \
+		"{run.root}/programs/{run.solver}" {run.args} \
+		-f "{run.file}" \
+		> runsolver.solver 2>&1 &
+}}
 
-[[ .finished -nt start.sh ]] || run
 
-touch .finished
+case $1 in 
+	run)
+		run
+		;;
+	run2)
+		run2
+		;;
+    *)    
+		cd "$(dirname $0)"
+		[[ .finished -nt start.sh ]] || /usr/bin/time -f "Real time (s): %e" -o runsolver.watcher "./$(basename $0)" run2
+		touch .finished
+		;;
+esac
