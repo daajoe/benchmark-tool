3251b4c00c6b7764680207660efb5663854d4e7a
rkaminski <rkaminski@c2a30a28-f9ed-4af8-86dc-e4e425da2d4f>
2010-03-23 12:21:55 +0000

    - fixed scripts to run on cluster
    git-svn-id: http://svn.code.sf.net/p/potassco/code/trunk/benchmark-tool@2446 c2a30a28-f9ed-4af8-86dc-e4e425da2d4f

--- templates/pbs-generic.sh
+++ templates/pbs-generic.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-cd "$(dirname $0)"
+function abort()
+{{
+	echo TIMEOUT!
+	kill $MPI_PID > /dev/null 2>&1
+}}
 
-[[ .finished -nt start.sh ]] || "{run.root}/programs/runsolver-3.2.5" \
-	-w runsolver.watcher \
-	-o runsolver.solver \
-	1> runsolver.stderr \
-	2> runsolver.stdout \
-	-W {run.timeout} \
+function run()
+{{
+	trap abort SIGUSR1
+	/usr/bin/time -f "Real time (s): %e" -o runsolver.watcher \
 	mpiexec -machinefile $PBS_NODEFILE -n $PBS_NODES \
 	"{run.root}/programs/{run.solver}" {run.args} \
-	< "{run.file}"
+	-f "{run.file}" \
+	> runsolver.solver 2>&1 &
+	export MPI_PID=$!
+	export BASH_PID=$$
+	(sleep $[{run.timeout}+10]; kill -SIGUSR1 $BASH_PID) &
+	sleep_pid=$!
+	wait $MPI_PID > /dev/null 2>&1
+	kill $sleep_pid
+}}
+
+cd "$(dirname $0)"
+
+[[ .finished -nt start.sh ]] || run
 
 touch .finished
