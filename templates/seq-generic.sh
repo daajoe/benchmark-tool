ce932430c449230db7ed55215918c6343a2f2cc7
rkaminski <rkaminski@c2a30a28-f9ed-4af8-86dc-e4e425da2d4f>
2010-07-27 15:21:41 +0000

    git-svn-id: http://svn.code.sf.net/p/potassco/code/trunk/benchmark-tool@3240 c2a30a28-f9ed-4af8-86dc-e4e425da2d4f

--- templates/seq-generic.sh
+++ templates/seq-generic.sh
@@ -2,13 +2,13 @@
 
 cd "$(dirname $0)"
 
-[[ -e .finished ]] || "{run.root}/programs/runsolver-3.2.5" \
+[[ -e .finished ]] || zcat "{run.file}" | "{run.root}/programs/runsolver-3.2.5" \
+	-M 100000 \
 	-w runsolver.watcher \
 	-o runsolver.solver \
 	1> runsolver.stderr \
 	2> runsolver.stdout \
 	-W {run.timeout} \
-	"{run.root}/programs/{run.solver}" {run.args} \
-	< "{run.file}"
+	"{run.root}/programs/{run.solver}" {run.args}
 
 touch .finished
