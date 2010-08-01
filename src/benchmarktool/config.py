b4f8b02bd9f73ba7f21cc942b6ea0392d985cd7d
rkaminski <rkaminski@c2a30a28-f9ed-4af8-86dc-e4e425da2d4f>
2010-08-01 13:50:22 +0000

    - runsolver prints non-unicode chars ahhrrgg - added functions to extract clasp statistics when run in sat mode
    git-svn-id: http://svn.code.sf.net/p/potassco/code/trunk/benchmark-tool@3254 c2a30a28-f9ed-4af8-86dc-e4e425da2d4f

--- src/benchmarktool/config.py
+++ src/benchmarktool/config.py
@@ -47,7 +47,8 @@ def clasp(root, runspec, instance):
         if m: interrupted = 1
     
     # parse runsolver output
-    for line in open(os.path.join(root, "runsolver.watcher")):
+    import codecs
+    for line in codecs.open(os.path.join(root, "runsolver.watcher"), errors='ignore', encoding='utf-8'):
         m = clasp_time.match(line)
         if m: time = float(m.group(1))
     
@@ -106,6 +107,66 @@ def smodels(root, runspec, instance):
     result.append(("status", "string", status))
     result.append(("time", "float", time))
 
+    return result
+
+clasp_sat_models      = re.compile(r"^c Models[ ]*:[ ]*([0-9]+)\+?[ ]*$")
+clasp_sat_choices     = re.compile(r"^c Choices[ ]*:[ ]*([0-9]+)\+?[ ]*$")
+clasp_sat_time        = re.compile(r"^Real time \(s\): ([0-9]+\.[0-9]+)$")
+clasp_sat_conflicts   = re.compile(r"^c Conflicts[ ]*:[ ]*([0-9]+)\+?[ ]*$")
+clasp_sat_restarts    = re.compile(r"^c Restarts[ ]*:[ ]*([0-9]+)\+?[ ]*$")
+clasp_sat_status      = re.compile(r"^s (SATISFIABLE|UNSATISFIABLE|UNKNOWN)[ ]*$")
+clasp_sat_interrupted = re.compile(r"^c INTERRUPTED[ ]*:[ ]*(1)[ ]*$")
+
+
+def clasp_sat(root, runspec, instance):
+    """
+    Extracts some clasp statistics (only for dimacs).
+    (This function was tested with clasp-1.3.4.)   
+    """
+    result      = []
+    interrupted = 0
+    status      = None
+    timeout     = time = runspec.project.job.timeout
+    # parse some of clasp's stats
+    for line in open(os.path.join(root, "runsolver.solver")):
+        m = clasp_sat_models.match(line)
+        if m: result.append(("models", "float", m.group(1)))
+        
+        m = clasp_sat_choices.match(line)
+        if m: result.append(("choices", "float", m.group(1)))
+        
+        m = clasp_sat_conflicts.match(line)
+        if m: result.append(("conflicts", "float", m.group(1)))
+        
+        m = clasp_sat_restarts.match(line)
+        if m: result.append(("restarts", "float", m.group(1)))
+        
+        m = clasp_sat_status.match(line)
+        if m: status =  m.group(1)
+        
+        m = clasp_sat_interrupted.match(line)
+        if m: interrupted = 1
+    
+    # parse runsolver output
+    for line in open(os.path.join(root, "runsolver.watcher")):
+        m = clasp_sat_time.match(line)
+        if m: time = float(m.group(1))
+    
+    # count suspicious stuff as timeout
+    if status == None:
+        result.append(("error", "float", 1))
+        sys.stderr.write("*** ERROR: Run {0}/{1} failed with unrecognized status!\n".format(instance.classname, instance.instance))
+    else:
+        result.append(("error", "float", 0))
+
+    if (status != "SATISFIABLE" and status != "UNSATISFIABLE") or time >= timeout:
+        time = timeout
+        result.append(("timeout", "float", 1))
+    else:
+        result.append(("timeout", "float", 0))
+    result.append(("status", "string", status))
+    result.append(("time", "float", time))
+    result.append(("interrupted", "float", interrupted))
     return result
 
 claspar_models    = re.compile(r"^Models[ ]*:[ ]*([0-9]+)\+?[ ]*$")
