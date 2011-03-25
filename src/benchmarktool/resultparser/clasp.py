30bea3aacd17fc01e131fe2266cdb7a66fa3493b
rkaminski <rkaminski@c2a30a28-f9ed-4af8-86dc-e4e425da2d4f>
2011-03-25 15:46:10 +0000

    git-svn-id: http://svn.code.sf.net/p/potassco/code/trunk/benchmark-tool@4172 c2a30a28-f9ed-4af8-86dc-e4e425da2d4f

--- src/benchmarktool/resultparser/clasp.py
+++ src/benchmarktool/resultparser/clasp.py
@@ -51,7 +51,7 @@ def clasp(root, runspec, instance):
     result.append(("memout", "float", int(memout)))
 
     if "optimum" in res and not " " in res["optimum"][1]:
-        result.append("optimum", "float", float(res["optimum"][1]))
+        result.append(("optimum", "float", float(res["optimum"][1])))
         del res["optimum"]
     if "interrupted" in res: del res["interrupted"]
     if "error" in res: del res["error"]
