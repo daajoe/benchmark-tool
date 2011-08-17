eaaf40da262b90e846f88cbbc10f98243512788e
manju_sama <manju_sama@c2a30a28-f9ed-4af8-86dc-e4e425da2d4f>
2011-08-17 15:01:44 +0000

    pinning processes to allocated ressources
    git-svn-id: http://svn.code.sf.net/p/potassco/code/trunk/benchmark-tool@4683 c2a30a28-f9ed-4af8-86dc-e4e425da2d4f

---
+++ templates/seq-genericPP.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CAT="{run.root}/programs/gcat.sh"
+
+cd "$(dirname $0)"
+
+ID=`echo $PBS_JOBID | sed  's/\([0-9]*\).*/\1/g'`
+
+MASK=`qstat -f $ID | python {run.root}/templates/mask.py`
+
+#top -n 1 -b > top.txt
+
+[[ -e .finished ]] || $CAT "{run.file}" | taskset $MASK "{run.root}/programs/runsolver-3.2.5" \
+	-M 20000 \
+	-w runsolver.watcher \
+	-o runsolver.solver \
+	-W {run.timeout} \
+	"{run.root}/programs/{run.solver}" {run.args}
+
+touch .finished
