3806082ce98203cd21f3181677ae39f08c2530fc
manju_sama <manju_sama@c2a30a28-f9ed-4af8-86dc-e4e425da2d4f>
2011-08-17 15:05:46 +0000

    old seq-generic.sh (ok for huat, jersey and so on)
    git-svn-id: http://svn.code.sf.net/p/potassco/code/trunk/benchmark-tool@4684 c2a30a28-f9ed-4af8-86dc-e4e425da2d4f

--- templates/seq-generic.sh
+++ templates/seq-generic.sh
@@ -6,7 +6,7 @@ cd "$(dirname $0)"
 
 #top -n 1 -b > top.txt
 
-[[ -e .finished ]] || $CAT "{run.file}" | taskset 0xfe "{run.root}/programs/runsolver-3.2.5" \
+[[ -e .finished ]] || $CAT "{run.file}" | "{run.root}/programs/runsolver-3.2.5" \
 	-M 20000 \
 	-w runsolver.watcher \
 	-o runsolver.solver \
