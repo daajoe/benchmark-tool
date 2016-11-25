a1ef838859abb1799baa2dc77ec22c9e556751a9
Johannes Fichte <daajoe@ficht.es>
2016-11-25 13:04:29 +0100

    fixed runtime arguments for temp folder + cleanup

--- templates/seq-tw.sh
+++ templates/seq-tw.sh
@@ -2,17 +2,19 @@
 # http://www.cril.univ-artois.fr/~roussel/runsolver/
 
 #CAT="{run.root}/programs/gcat.sh"
+source ~/.bashrc
 
 cd "$(dirname $0)"
 
 #top -n 1 -b > top.txt
 
-#[[ -e .finished ]] || $CAT "{run.file}" | "{run.root}/programs/runsolver-3.3.4" \
+printf 'hostname is ' >> runsolver.err
+hostname >> runsolver.err
+
 [[ -e .finished ]] ||  "{run.root}/programs/runsolver-3.3.1" \
 	-M 20000 \
 	-w runsolver.watcher \
-	#-o runsolver.solver \
-	#-W {run.timeout} \
-	"{run.root}/programs/{run.solver}" {run.args} -f "{run.file}" > runsolver.solver 2>runsolver.err
+	-W {run.timeout} \
+	"{run.root}/programs/{run.solver}" {run.args} -p $temp -f "{run.file}" > runsolver.solver 2>>runsolver.err
 
 touch .finished
