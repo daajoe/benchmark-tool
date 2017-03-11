4bd312b4b941f0cbdc9ae85b9c4e146d6ff255b7
Johannes Fichte <daajoe@ficht.es>
2017-03-11 17:13:38 +0100

    added run id parameter for evaluation

--- templates/condor/runsolver_template.py
+++ templates/condor/runsolver_template.py
@@ -58,7 +58,7 @@ args.stderr='{run.root}/{run.path}/{run.instancebase}.err'
 #args.stderr='/dev/stderr'
 args.finished = '{run.root}/{run.finished}'
 # args.debug=True
-
+args.run='{run.run}'
 
 if os.path.isfile(args.finished):
   sys.stderr.write('INSTANCE RUN "%s" already exists. Exiting...\n' %args.finished)
@@ -96,7 +96,7 @@ def main(args):
   sys.stderr.write(args.solver_args)
   sys.stderr.write('\n')
 
-  cmd = '%s -M %s -W %s -w %s %s %s -t %s -f %s > %s 2>> %s' % (args.runsolver, args.memlimit, args.timelimit, args.watcher, args.solver, ''.join(args.solver_args), tmp, ' '.join(args.filename), args.stdout, args.stderr)
+  cmd = '%s -M %s -W %s -w %s %s %s -t %s --runid %s -f %s > %s 2>> %s' % (args.runsolver, args.memlimit, args.timelimit, args.watcher, args.solver, ''.join(args.solver_args), tmp, args.run, ' '.join(args.filename), args.stdout, args.stderr)
   sys.stderr.write('COMMAND=%s\n' %cmd)
 
   p_solver = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True, close_fds=True)
