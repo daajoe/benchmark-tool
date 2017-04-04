5f83d9e75cb6530a1d02ba46800daee92dcedc65
Johannes Fichte <daajoe@ficht.es>
2017-04-04 20:24:02 +0200

    adaptions for behemoth cluster

--- templates/seq-tw-LI.sh
+++ templates/seq-tw-LI.sh
@@ -11,10 +11,14 @@ cd "$(dirname $0)"
 printf 'hostname is ' >> runsolver.err
 hostname >> runsolver.err
 
+if [ ! -v TMPDIR ] ; then
+  TMPDIR=$(dirname $(mktemp))
+fi
+
 [[ -e .finished ]] ||  "{run.root}/programs/runsolver-3.3.5" \
 	-M 8196 \
 	-w runsolver.watcher \
 	-W {run.timeout} \
-	"{run.root}/programs/{run.solver}" {run.args} -p $temp -f "{run.file}" > runsolver.solver 2>>runsolver.err
+	"{run.root}/programs/{run.solver}" {run.args} -t $TMPDIR -f "{run.file}" > runsolver.solver 2>>runsolver.err
 
 touch .finished
