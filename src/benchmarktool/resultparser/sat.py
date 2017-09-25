5fddc3b108563b37e81b6e4d75076e2cff95a98c
Johannes Fichte <daajoe@ficht.es>
2017-09-25 16:18:15 +0200

    Updated: default sat.py

--- src/benchmarktool/resultparser/sat.py
+++ src/benchmarktool/resultparser/sat.py
@@ -10,12 +10,25 @@ import sys
 import codecs
 from benchmarktool.tools import escape
 
-sat_re = {
-    "wall": ("float", re.compile(r"^Real time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
+watcher_re = {
+    "time": ("float", re.compile(r"^Real time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: round(x,4)),
+    "time_cpu": ("float", re.compile(r"^CPU time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
+    "cpuusage": ("float", re.compile(r"^CPU usage \(%\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: float(x)),
     "memerror": ("string", re.compile(r"^Maximum VSize (?P<val>exceeded): sending SIGTERM then SIGKILL"), lambda x: x),
-    "memusage": ("float", re.compile(r"^maximum resident set size= (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: round(x / 1024,2))
+    "memusage": ("float", re.compile(r"^maximum resident set size= (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: round(x/1024, 2)),
+    "memusage_vmem": ("float", re.compile(r"^Max\. virtual memory \(cumulated for all children\) \(KiB\):\s*(?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: round(x/1024,2)),
+    "status_retcode": ("float", re.compile(r"^Child status: (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: int(x)),
+    "context_switches_involuntary":  ("int", re.compile(r"^involuntary context switches=\s*(?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: int(x)),
+    "context_switches_voluntary":  ("int", re.compile(r"^voluntary context switches=\s*(?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: int(x)),                                      
 }
 
+solver_re = {
+    "status": ("string", re.compile(r"^s\s*(?P<val>SATISFIABLE|UNSATISFIABLE|UNKNOWN)\s*$"), lambda x: x)
+}
+
+
+#see: http://www.satcompetition.org/2004/format-solvers2004.html
+sat_return_codes = (10, 20)
 
 def sat(root, runspec, instance):
     """
@@ -27,20 +40,25 @@ def sat(root, runspec, instance):
         'setting': ('string', runspec.setting.name),
         'timelimit': ('int', runspec.project.job.timeout),
         'memlimit': ('int', runspec.project.job.memout),
-        'memerror': ('string', 'NA'),
         'solver': ('string', runspec.system.name),
         'solver_version': ('string', runspec.system.version),
         'solver_args': ('string', escape(runspec.setting.cmdline)),
         'full_call': ('string', None),
         'full_path': ('string', root),
         'hash': ('string', None),
-        'solved': ('int', -1),
-        'wall': ('float', 0),
+        'solved': ('int', 0),
+        'time': ('float', 0),
+        'cpuusage': ('float', 0),
         'memusage': ('float', 0),
+        'memusage_vmem': ('float', 0),
+        'context_switches_involuntary': ('int', 0), 
+        'context_switches_voluntary': ('int', 0),
         'run': ('int', 0),
         'error': ('int', 0),
         'error_str': ('string', ""),
-        'stderr': ('string', "")
+        'stderr': ('string', ""),
+        'status': ('string', "NA"),
+        'status_retcode': ('int', 0)
     }
 
     finished = True
@@ -54,42 +72,52 @@ def sat(root, runspec, instance):
     try:
         for f in ['%s.watcher' % instance_str]:
             for line in codecs.open(os.path.join(root, f), encoding='utf-8'):
-                for val, reg in sat_re.items():
+                for val, reg in runsolver_re.items():
                     m = reg[1].match(line)
-                    if m: res[val] = (reg[0], res[2](float(m.group("val"))) if reg[0] == "float" else m.group("val"))
+                    #always take the max instead of the last value
+                    if m:
+                        m_value = (reg[0], reg[2](float(m.group("val"))) if reg[0] == "float" else m.group("val"))
+                        if val in res:
+                            res[val] = (m_value[0], max(res[val][1], m_value[1]))
+                        else:
+                            res[val] = m_value
 
-        if "memerror" in res and res['memerror'] != 'NA':
+        if "memerror" in res:
             res["error_str"] = ("string", "std::bad_alloc")
-            res["error"] = ("int", 2)
-            res["status"] = ("string", "UNKNOWN")
+            res["error"] = ("int", res["error"] + 2)
             del res["memerror"]
 
     except IOError, e:
         finished = False
         res['error_str'] = ('string', 'Did not finish.')
 
-    if finished:
+    if finished:        
         content = codecs.open(os.path.join(root, '%s.txt' % instance_str), errors='ignore', encoding='utf-8').read()
         content = content.replace(",\n]", "\n]", 1)
 
+        for line in content.split('\n'):
+            for val, reg in solver_re.items():
+                m = reg[1].match(line)
+                #always take the max instead of the last value
+                if m:
+                    m_value = (reg[0], reg[2](float(m.group("val"))) if reg[0] == "float" else m.group("val"))
+                    if val in res:
+                        res[val] = (m_value[0], max(res[val][1], m_value[1]))
+                    else:
+                        res[val] = m_value
 
-
-        result = []
-        error = not "status" in res or ("error" in res and res["error"][1] != "std::bad_alloc")
+        res["solved"] = ("int", int(res["status_retcode"][1] in sat_return_codes))
+        error_status = not "status" in res or res["status"][1] == -1
         memout = "error" in res and res["error"][1] == "std::bad_alloc"
-        status = res["status"][1] if "status" in res else None
-
-        timedout = memout or error or status == "UNKNOWN" or (status == "SATISFIABLE" and "optimum" in res) or \
-                   res["time"][
-                       1] >= timeout or "interrupted" in res;
+        timedout = memout or error_status or not res["solved"] or  res["time"][1] >= timeout
+        
         if timedout: res["istimeout"] = ("float", timeout)
-        if error: sys.stderr.write("*** ERROR: Run {0} failed with unrecognized status or error!\n".format(root))
+        if error_status:
+            sys.stderr.write("*** ERROR: Run {0} failed with unrecognized status or error!\n".format(root))
+            sys.stderr.write(str(res))
+            sys.stderr.write('\n')
+            exit(1)
 
-        if "optimum" in res and not " " in res["optimum"][1]:
-            result.append(("optimum", "float", float(res["optimum"][1])))
-            del res["optimum"]
         if "interrupted" in res: del res["interrupted"]
-        if "error" in res: del res["error"]
-        for key, val in res.items(): result.append((key, val[0], val[1]))
 
     return [(key, val[0], val[1]) for key, val in res.items()]
