13e38a849fafbf65306ab55656c7d9dd1f766929
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2019-01-11 11:30:46 +0000

    Add new team6 solver with wrapper

---
+++ programs/sudoku_team6/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE=$(readlink -f -- "$0")
+DIR=$(dirname "$FILE")
+$DIR/sudoku-sat $1 1> /dev/null && cat output.txt
+
+
+# Clean up
+# RESIDUAL=(CNF.cnf output.txt)
+# for file in ${RESIDUAL[*]}
+# do
+#     test -e $file && rm $file
+# done
