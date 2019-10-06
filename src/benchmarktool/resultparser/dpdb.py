ce3ff8950eb87a75a2009d1715c49da16d453273
Johannes Fichte <daajoe@ficht.es>
2019-10-06 20:04:40 +0200

    Minors

--- src/benchmarktool/resultparser/dpdb.py
+++ src/benchmarktool/resultparser/dpdb.py
@@ -4,34 +4,47 @@ Created on Mar 18, 2017
 @author: Johannes K. Fichte
 '''
 
+import sys
+
 import codecs
-import json
 import os
 import re
-import sys
 from base64 import b64encode
-
 from benchmarktool.tools import escape
 
+# Child status: 0
 runsolver_re = {
-    "wall": ("float", re.compile(r"^Real time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
+    "time": ("float", re.compile(r"^Real time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
+    "cputime": ("float", re.compile(r"^CPU time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
+    "cpuusertime": ("float", re.compile(r"^CPU user time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
+    "cpusystime": (
+        "float", re.compile(r"^CPU usage (%): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
+    "cpuusage": (
+        "float", re.compile(r"^CPU system time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
     "memerror": ("string", re.compile(r"^Maximum VSize (?P<val>exceeded): sending SIGTERM then SIGKILL"), lambda x: x),
     "segfault": (
         "string", re.compile(r"^\s*Child\s*ended\s*because\s*it\s*received\s*signal\s*11\s*\((?P<val>SIGSEGV)\)\s*"),
         lambda x: x),
-    "time": ("float", re.compile(r"^Real time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
     "memusage": (
         "float", re.compile(r"^maximum resident set size= (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: round(x / 1024, 2)),
     "status": ("int", re.compile(r"Child status: (?P<val>[0-9]+)"), lambda x: x),
-    "cumcpu_time": (
-    "float", re.compile("Current children cumulated CPU time \(s\)\s(?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x)
 }
 
 dpdb_re = {
-    "time": ("float", re.compile(r"^Real time \(s\): (?P<val>[0-9]+(\.[0-9]+)?)$"), lambda x: x),
-    "memerror": ("string", re.compile(r"^Maximum VSize (?P<val>exceeded): sending SIGTERM then SIGKILL"), lambda x: x),
-    "objective": ("float", re.compile(r"^\[INFO\] dpdb\.problems\.sharpsat: Problem has (?P<val>[0-9]+) models"), lambda x: x)
+    "objective": (
+    "float", re.compile(r"^\[INFO\] dpdb\.problems\.sharpsat: Problem has (?P<val>[0-9]+) models"), lambda x: x)
 }
+
+dpdb_err_re = {
+    "err_db": ("float", re.compile(r"^\[INFO\] dpdb.problem (?P<val>)"), lambda x: 1),
+    "err_db_conn": ("string", re.compile(r"^\[WARNING\] dpdb: Killing all connections(?P<val>)"), lambda x: 1),
+    "err_thread": ("string", re.compile(r"^\[ERROR\] dpdb.problem: Error in worker thread(?P<val>)"), lambda x: 1),
+    "err_parse_no_type": (
+    "string", re.compile(r"^\[ERROR\] dpdb.reader: No type found in DIMACS file!(?P<val>)"), lambda x: 1),
+    "err_parse_reader": (
+    "string", re.compile(r"^\[WARNING\] dpdb.reader: Invalid content in preamble at line 0:(?P<val>)"), lambda x: 1),
+}
+
 
 def dpdb(root, runspec, instance):
     """
@@ -57,7 +70,7 @@ def dpdb(root, runspec, instance):
         'num_hyperedges': ('int', 'nan'),
         'hash': ('string', None),  # TODO:
         'solved': ('int', 0),
-        'wall': ('float', 0),
+        'time': ('float', 0),
         'run': ('int', 0),
         'objective': ('float', 'nan'),
         'error': ('int', 0),
@@ -76,70 +89,53 @@ def dpdb(root, runspec, instance):
     dbconn_warning = False
     thread_err = False
 
-    #TODO: next
-
     # ERROR HANDLING
     try:
         logfile = os.path.join(root, '%s.err' % instance_str)
         err_content = codecs.open(logfile, errors='ignore', encoding='utf-8').read()
         err_content = err_content.replace(",\n]", "\n]", 1)
         for line in err_content.splitlines():
-            if not interrupt_err and '[INFO] dpdb.problem' in err_content and 'interrupted' in line:
-                valid_solver_run = False
-                interrupt_err = True
-                res['error_str'] = ('string', res['error_str'][1] + "Python Runtime error: check logfile %s\n" %
-                                    "[INFO] dpdb.problem node ... interrupted")
-            if not dbconn_warning and '[WARNING] dpdb: Killing all connections' in line:
-                dbconn_warning = True
-                res['error_str'] = ('string', res['error_str'][1] + "Python Runtime error: check logfile %s\n" %
-                                    "[WARNING] dpdb: Killing all connections")
-            if not thread_err and '[ERROR] dpdb.problem: Error in worker thread' in line:
-                thread_err = True
-                res['error_str'] = ('string', res['error_str'][1] + "Python Runtime error: check logfile %s\n" %
-                                    "[ERROR] dpdb.problem: Error in worker thread")
+            for val, reg in dpdb_err_re.items():
+                m = reg[1].match(line)
+                if m: res[val] = (reg[0], reg[2](m.group("val")))
+
             # noinspection DuplicatedCode
             for val, reg in dpdb_re.items():
                 m = reg[1].match(line)
-                if m: res[val] = (reg[0], reg[2](float(m.group("val"))) if reg[0] == "float" else m.group("val"))
+                if m: res[val] = (reg[0], reg[2](m.group("val")))
 
     except IOError:
         sys.stderr.write('Missing Error file for instance %s.\n' % root)
 
     runsolver_error = False
     # WATCHER DATA HANDLING
-    if pbs_job:
+    try:
+        f = codecs.open(os.path.join(root, '%s.watcher' % instance_str), errors='ignore', encoding='utf-8').read()
+        for line in f.splitlines():
+            for val, reg in runsolver_re.items():
+                m = reg[1].match(line)
+                if m: res[val] = (reg[0], reg[2](float(m.group("val"))) if reg[0] == "float" else m.group("val"))
+
+        if res['memerror'][1] != 'nan':
+            res["error_str"] = ("string", "std::bad_alloc")
+            res["error"] = ("int", 2)
+            res['memout'] = ("int", 1)
+        else:
+            res['memout'] = ("int", 0)
+
         try:
-            f = codecs.open(os.path.join(root, '%s.watcher' % instance_str), errors='ignore', encoding='utf-8').read()
-            for line in f.splitlines():
-                for val, reg in runsolver_re.items():
-                    m = reg[1].match(line)
-                    if m: res[val] = (reg[0], reg[2](float(m.group("val"))) if reg[0] == "float" else m.group("val"))
+            res['timeout'] = ('int', int(res["time"][1] >= res['timelimit'][1]))
+        except KeyError, _:
+            res['timeout'] = ('int', int(res["time"][1] >= res['timelimit']))
+            runsolver_error = True
 
-            if res['memerror'][1] != 'nan':
-                res["error_str"] = ("string", "std::bad_alloc")
-                res["error"] = ("int", 2)
-                res['memout'] = ("int", 1)
-            else:
-                res['memout'] = ("int", 0)
-
-            try:
-                res['timeout'] = ('int', int(res["time"][1] >= res['timelimit'][1]))
-                if 'cumcpu_time' in res:
-                    del res['cumcpu_time']
-            except KeyError, e:
-                # sometimes we obtain lost times during runs
-                # we report them here but note that there was a runsolver error
-                res['time'] = res['cumcpu_time']
-                res['timeout'] = ('int', int(res["time"][1] >= res['timelimit']))
-                runsolver_error = True
-
-            res['finished'] = ('int', int(res['status'][1] >= 0))
-            # to = log_content.find('Child ended because it received signal 15 (SIGTERM)') >=0
-            if not res['finished']:
-                sys.stderr.write('instance %s did not finish properly\n' % root)
-        except IOError:
-            res['error_str'] = ('string', 'Did not finish.')
-            return [(key, val[0], val[1]) for key, val in res.items()]
+        res['finished'] = ('int', int(res['status'][1] >= 0))
+        # to = log_content.find('Child ended because it received signal 15 (SIGTERM)') >=0
+        if not res['finished']:
+            sys.stderr.write('instance %s did not finish properly\n' % root)
+    except IOError:
+        res['error_str'] = ('string', 'Did not finish.')
+        return [(key, val[0], val[1]) for key, val in res.items()]
 
     else:
         log_content = open(os.path.join(root, "condor.log")).read()
@@ -170,28 +166,25 @@ def dpdb(root, runspec, instance):
     # except IOError:
     #     sys.stderr.write('Instance %s did not finish properly. Missing output file.\n' % root)
     #     cluster_error = True
+    def set_bits(res, values):
+        bits = []
+        for i,val in enumerate(values):
+            if res.has_key(val) and res[val][1] != 0:
+                bits.append(1)
+            else:
+                bits.append(0)
+        bitstr = ''.join(map(str,bits))
+        return int(bitstr,2)
 
-    # error codes: 0 = ok, 1 = timeout, 2 = memout, 64 = invalid output, 128 = program runtime error,
-    # 256 = runsolver error/glitch, 512 = error signal handling, 1024 = cluster run error
-    # 2048 = invalid result, 4096 = cplex license issue
-    error += int(res['timeout'][1])
-    error += int(res['memout'][1]) * 2
-    # error += int(not valid_json) * 64
-    error += int(not valid_solver_run) * 128
-    error += int(runsolver_error) * 256
-    error += int(interrupt_err) * 512
-    error += int(dbconn_warning) *1024
-    error += int(thread_err) * 2048
-
+    values = ['timeout', 'memout', 'err_db', 'err_db_conn', 'err_thread', 'err_parse_no_type', 'err_parse_reader']
+    error = set_bits(res, values)
     res['error'] = ('int', error)
 
     try:
-        res['wall'] = ('float', stats['wall'])
-        res['solved'] = ('int', int(stats['solved']))
+        if res['objective'][1] != 'nan':
+            res['solved'] = ('int', 1)
     except KeyError, e:
         pass
-
-    #TODO: twins
 
     def nan_or_value(tpl, stats):
         try:
@@ -201,7 +194,7 @@ def dpdb(root, runspec, instance):
 
     # PROBLEM/SOLVER SPECIFIC OUTPUT
     if error == 0:
-        #TODO: fixme
+        # TODO: fixme
         if not res.has_key('objective'):
             res['objective'] = 'nan'
 
